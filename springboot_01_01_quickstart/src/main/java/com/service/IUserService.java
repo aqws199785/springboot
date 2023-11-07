5154fc998e23a92e66b0ae242f23adf76df7bee1
sort <2295375633@qq.com>
2023-11-07 16:13:16 +0800

    Initial commit

---
+++ springboot_01_01_quickstart/src/main/java/com/service/IUserService.java
@@ -0,0 +1,10 @@
+package com.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.bean.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IUserService extends IService<User> {
+    result
+}
